412557d6b7d4aa767cb1169ab569c367105ced64
weianke <245783065@qq.com>
2021-04-20 14:59:39 +0800

    新增

--- mather.test.js
+++ mather.test.js
@@ -1,25 +1,78 @@
 import { add, minus, multi } from './math.js';
 
-test('测试加法', () => {
-  expect(add(2, 4)).toBe(6);
+// test('toBe 匹配器', () => {
+//   // toBe匹配器
+//   expect(10).toBe(10);
+// });
+
+// test('toEqual 匹配器', () => {
+//   // toequal 判断内容相等
+//   const a = {
+//     one: 1,
+//   };
+//   expect(a).toEqual({
+//     one: 1,
+//   });
+// });
+
+// test('toBeUndefined 匹配器', () => {
+//   // toequal 判断内容相等
+//   const a = undefined;
+//   expect(a).toBeUndefined();
+// });
+
+// test('toBeDefined 匹配器', () => {
+//   // toequal 判断内容相等
+//   const a = 1;
+//   expect(a).toBeDefined();
+// });
+
+
+// test('toBeTruthy 匹配器', () => {
+//   // toequal 判断内容相等
+//   const a = 1;
+//   expect(a).toBeTruthy();
+// });
+
+test('toBeFalsy 匹配器', () => {
+  // toequal 判断内容相等
+  const a = 1;
+  expect(a).not.toBeFalsy();
 });
 
-test('测试减法', () => {
-  expect(minus(2, 4)).toBe(-2);
+// // 数字相关的匹配器打分
+// test('测试0.1+0.2', () => {
+//   const a = 0.1;
+//   const b = 0.2;
+//   expect(a + b).toBeCloseTo(0.3)
+// })
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/top/);
 });
 
-test('测试乘法', () => {
-  expect(multi(2, 4)).toBe(8);
+// const shoppingList = [
+//   'diapers',
+//   'kleenex',
+//   'trash bags',
+//   'paper towels',
+//   'milk',
+// ];
+
+// test('the shopping list has milk on it', () => {
+//   expect(shoppingList).toContain('trash bags');
+//   expect(new Set(shoppingList)).toContain('milk');
+// });
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
 });
-
-
-// 单元测试、集成测试
-// 模块测试、多个模块测试
-
-// 1.npm run jest
-// 2.jest 執行（babel-jest）
-// 3. babel-core
-// 4.在運行測試之前，集合babel, 先把你的代碼做一次轉化
-// 5. 運行轉化過的測試用例代碼
-
-
